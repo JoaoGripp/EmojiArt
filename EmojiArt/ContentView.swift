3a4a392bd3a1edf7f7685ab59776b60b4f30a7e7
JoaoGripp <corguinha@gmail.com>
2022-08-11 16:57:52 -0300

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Joao Gripp on 11/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
