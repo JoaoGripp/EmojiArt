11d69ad321e2e07bae61af4830139d86f5dd8705
JoaoGripp <corguinha@gmail.com>
2022-08-15 15:52:53 -0300

    add Persistente on the App

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -11,16 +11,50 @@ class EmojiArtDocument: ObservableObject {
     
     @Published private(set) var emojiArt: EmojiArtModel {
         didSet {
+            autosave()
             if emojiArt.background != oldValue.background {
                 fetchBackgroundImageDataIfNecessary()
             }
         }
     }
+    private struct Autosave {
+        static let filename = "Autosaved.emojiart"
+        static var url: URL? {
+            let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+            return documentDirectory?.appendingPathComponent(filename)
+        }
+    }
+    
+    private func autosave() {
+        if let url = Autosave.url {
+            save(to: url)
+        }
+    }
+    
+    private func save(to url: URL) {
+        let thisfunction = "\(String(describing: self)).\(#function)"
+        do {
+            let data: Data = try emojiArt.json()
+            print("\(thisfunction) json = \(String(data: data, encoding: .utf8) ?? "nil")")
+            try data.write(to: url)
+            print("\(thisfunction) success")
+        } catch let encodingError where encodingError is EncodingError {
+            print("\(thisfunction) couldn't encode EmojiArt as JSON because: \(encodingError.localizedDescription)")
+        } catch {
+            print("\(thisfunction) error: \(error)")
+        }
+        
+    }
     
     init() {
-        emojiArt = EmojiArtModel()
-        emojiArt.addEmoji("🦜", at: (-200, -100), size: 80)
-        emojiArt.addEmoji("🐇", at: (50, 100), size: 40)
+        if let url = Autosave.url, let autosaveEmojiArt = try? EmojiArtModel(url: url) {
+            emojiArt = autosaveEmojiArt
+            fetchBackgroundImageDataIfNecessary()
+        } else {
+            emojiArt = EmojiArtModel()
+        }
+//        emojiArt.addEmoji("🦜", at: (-200, -100), size: 80)
+//        emojiArt.addEmoji("🐇", at: (50, 100), size: 40)
     }
     
     var emojis: [EmojiArtModel.Emoji] { emojiArt.emojis }
